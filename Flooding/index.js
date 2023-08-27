6044bfcfff2d4f11654536e087e6348011cc11ff
andresdlRoca <andresdelaroca11@gmail.com>
2023-08-27 12:58:01 -0600

    Refactoring Flooding algorithm program

--- Flooding/index.js
+++ Flooding/index.js
@@ -1,88 +1,45 @@
-class Node {
-    constructor(id) {
-        this.id = id;
-        this.neighbors = [];
-        this.routingTable = {};
-    }
-
-    addNeighbor(node) {
-        this.neighbors.push(node);
-    }
-
-    addRoutingTableEntry(source, table) {
-        this.routingTable[source] = table;
-    }
-
-    receiveMessage(message) {
-        console.log("Node " + this.id + " received message " + message.content);
-        if(message.destination === this.id) {
-            console.log("Message reached destination");
-            return;
-        }
-
-        for (const neightbor of this.neighbors) {
-            neightbor.sendMessage(message);
-        }
-
-    }
-
-
-    sendMessage(message) {
-        console.log("Node " + this.id + " sending message " + message.content + " to " + message.destination);
-        for (const neightbor of this.neighbors) {
-            if (neightbor.id !== message.origin) {
-                neightbor.receiveMessage(message);
-        }
-    }
-    }
-
-}
-
-class Message {
-    constructor(origin, destination, content) {
-        this.origin = origin;
-        this.destination = destination;
-        this.content = content;
-    }
-}
-
-//Creating nodes
-const node1 = new Node('A');
-const node2 = new Node('B');
-const node3 = new Node('C');
-const node4 = new Node('D');
-
-//Adding neighbors
-node1.addNeighbor(node2);
-node1.addNeighbor(node3);
-node2.addNeighbor(node1);
-node2.addNeighbor(node4);
-node3.addNeighbor(node1);
-node3.addNeighbor(node4);
-node4.addNeighbor(node2);
-node4.addNeighbor(node3);
-
-// Initialize routing tables
-node1.addRoutingTableEntry(node1.id, {}); // Node 1 knows how to reach itself
-node1.addRoutingTableEntry(node2.id, {node2: node2});
-node1.addRoutingTableEntry(node3.id, {node3: node3});
-node1.addRoutingTableEntry(node4.id, {node4: node4});
-
-node2.addRoutingTableEntry(node1.id, {node1: node1});
-node2.addRoutingTableEntry(node2.id, {});
-node2.addRoutingTableEntry(node3.id, {});
-node2.addRoutingTableEntry(node4.id, {node4: node4});
-
-node3.addRoutingTableEntry(node1.id, {node1: node1});
-node3.addRoutingTableEntry(node2.id, {});
-node3.addRoutingTableEntry(node3.id, {});
-node3.addRoutingTableEntry(node4.id, {node4: node4});
-
-node4.addRoutingTableEntry(node1.id, {node1: node1});
-node4.addRoutingTableEntry(node2.id, {node2: node2});
-node4.addRoutingTableEntry(node3.id, {node3: node3});
-node4.addRoutingTableEntry(node4.id, {});
-
-// Sending messages
-const message = new Message(node1.id, node4.id, "Hello");
-node1.sendMessage(message);
+// Definición de nodos con sus identificadores
+const nodes = [
+    { id: 1, neighbors: [2, 3] },
+    { id: 2, neighbors: [1, 3] },
+    { id: 3, neighbors: [1, 2, 4] },
+    { id: 4, neighbors: [3] }
+  ];
+  
+  // Tablas de enrutamiento para cada nodo (inicialmente vacías)
+  const routingTables = {};
+  
+  // Inicialización de las tablas de enrutamiento
+  nodes.forEach(node => {
+    routingTables[node.id] = { [node.id]: node.id }; // Cada nodo puede enrutar a sí mismo
+  });
+  
+  // Función para enviar un mensaje a todos los nodos vecinos
+  function floodMessage(senderId, message) {
+    const senderNode = nodes.find(node => node.id === senderId);
+    senderNode.neighbors.forEach(neighborId => {
+      if (!routingTables[senderId][neighborId]) {
+        routingTables[senderId][neighborId] = senderId;
+        console.log(`Se agregó ruta desde ${senderId} hacia ${neighborId}`);
+      }
+      sendMessage(senderId, neighborId, message);
+    });
+  }
+  
+  // Función para enviar un mensaje de un nodo a otro
+  function sendMessage(senderId, receiverId, message) {
+    const receiverNode = nodes.find(node => node.id === receiverId);
+    console.log(`Mensaje enviado desde ${senderId} hacia ${receiverId}: ${message}`);
+    
+    // Simulación de recepción del mensaje por el nodo receptor
+    receiveMessage(receiverId, senderId, message);
+  }
+  
+  // Función para recibir un mensaje en un nodo
+  function receiveMessage(receiverId, senderId, message) {
+    console.log(`Mensaje recibido en nodo ${receiverId} desde nodo ${senderId}: ${message}`);
+  }
+  
+  // Simulación de envío de mensaje desde el nodo 1 al nodo 4
+  floodMessage(1, "Hola, nodo 4!");
+  
